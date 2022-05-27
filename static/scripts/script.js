6c348e541a80cd8cae967e33630255a81c31fbd7
farrahton <farrahtonpiers@hotmail.com>
2022-05-27 13:26:17 +0200

    :sparkles: previous button

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -8,47 +8,59 @@ function $(element) {
 // ===========
 
 // Start position onboarding.
-let onboardingIndex = 1; 
+let onboarding_index = 1; 
 
-const onboardingNext = $(`.next_button`)
+const onboarding_next = $(`.next_button`)
 
-onboardingNext.addEventListener("click", function(){
- if (onboardingIndex == 3) {
+onboarding_next.addEventListener("click", function(){
+ if (onboarding_index == 3) {
     // TO-DO: fix the fact that it only changes it to vragenlijst after click.
    $(".next_button").innerHTML = "Vragenlijst";
    window.location.href = "/";
  }
  else {
-    $(`.onboarding li:nth-child(${onboardingIndex})`).classList.remove("show_element")
-    onboardingIndex++
-    $(`.onboarding li:nth-child(${onboardingIndex})`).classList.add("show_element")
+    $(`.onboarding li:nth-child(${onboarding_index})`).classList.remove("show_element")
+    onboarding_index++
+    $(`.onboarding li:nth-child(${onboarding_index})`).classList.add("show_element")
  }
 })
+
 
 // ==============
 // progress steps
 // ==============
 
 // Start position questionnaire.
-let questionnaireIndex = 1; 
+let questionnaire_index = 1; 
 
-const questionnaireNext = $(`.next_button`)
-// 
+const questionnaire_next = $(`.next_button`)
+const questionnaire_prev = $(`.prev_button`)
 
 $(".questionnaire li:nth-child(1)").classList.add("show_element")
 
-questionnaireNext.addEventListener("click", function(){
- if (questionnaireIndex == 6) {
-    // Go to different route but idk how in clientside javascript.
-    questionnaireNext.classList.add("disable_button")
+questionnaire_next.addEventListener("click", function(){
+ if (questionnaire_index == 6) {
+    questionnaire_next.classList.add("disable_button")
  }
  else {
-    $(`.questionnaire li:nth-child(${questionnaireIndex})`).classList.remove("show_element")
-    questionnaireIndex++
+    $(`.questionnaire li:nth-child(${questionnaire_index})`).classList.remove("show_element")
+    questionnaire_index++
    //  TO-DO: a counter for the corresponding question number.
-    $(`.questionnaire li:nth-child(${questionnaireIndex})`).classList.add("show_element")
+    $(`.questionnaire li:nth-child(${questionnaire_index})`).classList.add("show_element")
  }
 })
+
+questionnaire_prev.addEventListener("click", function(){
+   if (questionnaire_index == 6) {
+      questionnaire_next.classList.add("disable_button")
+   }
+   else { 
+      $(`.questionnaire li:nth-child(${questionnaire_index})`).classList.remove("show_element")
+      questionnaire_index--
+     //  TO-DO: a counter for the corresponding question number.
+      $(`.questionnaire li:nth-child(${questionnaire_index})`).classList.add("show_element")
+      }
+  })
 
 
 //  TO-DO: make the previous button work, maybe add a skip option & implement local storage the onboarding and questionnaire so only new users get to see it.
