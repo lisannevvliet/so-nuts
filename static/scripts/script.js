8392bf3ee6dbec8e6879414ddc5f2219b01984a9
farrahton <farrahtonpiers@hotmail.com>
2022-05-27 02:05:13 +0200

    :sparkles: onboarding :bookmark: 1

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,3 +2,26 @@
 function $(element) {
     return document.querySelector(element)
 }
+
+// ===========
+// onboarding (module later)
+// ===========
+
+let onboardingIndex = 1; // start positie
+
+const onboardingNext = $(`.next_button`)
+
+onboardingNext.addEventListener("click", function(){
+ if (onboardingIndex == 3) {
+    // Go to different route but idk how in clientside javascript.
+    onboardingNext.classList.add("disable_button")
+ }
+ else {
+    $(`ol>li:nth-child(${onboardingIndex})`).classList.remove("show_li")
+    onboardingIndex++
+    $(`ol>li:nth-child(${onboardingIndex})`).classList.add("show_li")
+ }
+})
+
+
+
