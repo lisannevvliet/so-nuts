c74e7b352765ac8f31a3dfb8d62e65439179e05a
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-06-01 10:53:23 +0200

    Improve indentation of dashboard files

--- static/scripts/modules/dashboard.js
+++ static/scripts/modules/dashboard.js
@@ -1,18 +1,18 @@
 import $ from "./$.js"
 import $$ from "./$$.js"
 
-export function navigation_click() { 
+export function navigation_click() {
     if ($(".dashboard")) {
         var btns = document.getElementsByTagName("a");
 
-    for(var i=0; i<btns.length; i++){ 
-        btns[i].addEventListener("click", function() {
-            var current = document.getElementsByClassName("active")
-            current[0].className = current[0].className.replace("active")
-            this.className += ""
-        })
+        for (var i = 0; i < btns.length; i++) {
+            btns[i].addEventListener("click", function () {
+                var current = document.getElementsByClassName("active")
+                current[0].className = current[0].className.replace("active")
+                this.className += ""
+            })
 
-    }
+        }
     }
 }
 
