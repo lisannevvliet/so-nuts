1e2d3972ec7f56f4e08a8742e20ad392b4e9ab49
farrahton <farrahtonpiers@hotmail.com>
2022-06-02 11:37:50 +0200

    new routes

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,7 +2,7 @@ import $ from "./modules/$.js"
 import onboarding from "./modules/onboarding.js"
 import questionnaire from "./modules/questionnaire.js"
 // import dashboard from "./modules/dashboard.js"
-import profile from "./modules/profile.js"
+import profile from "./modules/dashboard_app/profile.js"
 
 // Check if the onboarding is currently displayed.
 if ($(".onboarding")) {
