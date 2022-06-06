e236893ebd24cc26b1facf2b319a5f45e914b5ed
Farrahton <farrahtonpiers@hotmail.com>
2022-06-06 13:09:33 +0200

    :truck: renamed dashboard to navigation

---
+++ static/scripts/modules/navigation.js
@@ -0,0 +1,12 @@
+import $$ from "./$$.js"
+
+export default function navigation() {
+    // function active_link() {
+    //     $$(".list").forEach((item) =>
+    //         item.classList.remove("active"))
+    //     this.classList.add("active")
+    // }
+
+    // $$(".list").forEach((item) =>
+    //     item.addEventListener("click", active_link))
+}
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,7 +1,7 @@
 import $ from "./modules/$.js"
 import onboarding from "./modules/onboarding.js"
 import questionnaire from "./modules/questionnaire.js"
-import dashboard from "./modules/dashboard.js"
+// import navigation from "./modules/navigation.js"
 
 // Check if the onboarding is currently displayed.
 if ($(".onboarding")) {
@@ -14,6 +14,6 @@ if ($(".questionnaire")) {
 }
 
 // Check if the dashboard is currently displayed.
-if ($(".dashboard")) {
-    // dashboard()
+if ($("nav")) {
+    // navigation()
 }
