2fd75f63db7a8d2b33fa45b14ba34c2fa142706f
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-06-13 10:16:19 +0200

    Put get function in different module

---
+++ modules.js
@@ -0,0 +1,22 @@
+// Import Node.js module.
+const fs = require("fs")
+
+module.exports = {
+    get: async function (name, url) {
+        // Try to get the data from the API. If it fails, use the JSON.
+        try {
+            // To use the JSON, comment out this line of code.
+            const response = await fetch(`https://fhir.mibplatform.nl/api/${url}`)
+            const data = await response.json()
+
+            // Save the most recent data in the JSON.
+            fs.writeFileSync(`static/json/${name}.json`, JSON.stringify(data))
+        } catch { }
+
+        // Read the JSON with the most recent data available.
+        const data = fs.readFileSync(`static/json/${name}.json`)
+
+        // Return the data in a JSON format.
+        return JSON.parse(data)
+    }
+}
