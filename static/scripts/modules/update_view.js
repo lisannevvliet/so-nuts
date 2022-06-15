ee6b28613b1459330e60c2214db3f027f16b8604
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-06-15 20:00:48 +0200

    Rename questionnaire_index to index

--- static/scripts/modules/update_view.js
+++ static/scripts/modules/update_view.js
@@ -2,12 +2,12 @@ import $ from "./$.js"
 import $$ from "./$$.js"
 import { load_answers } from "./answers.js"
 
-export default function update_view(questionnaire_index) {
+export default function update_view(index) {
     // Fill in the progress bar.
-    $$("#progress div")[questionnaire_index - 1].style.width = (questionnaire_index - 1) * 100 / $("#amount_of_questions").textContent + "%"
+    $$("#progress div")[index - 1].style.width = (index - 1) * 100 / $("#amount_of_questions").textContent + "%"
 
     // Check if there are any answers stored in localStorage.
     if (localStorage.getItem("answers")) {
-        load_answers(questionnaire_index)
+        load_answers(index)
     }
 }
