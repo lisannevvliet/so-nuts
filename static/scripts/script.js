09a72d3f60722d591f5edfdd284481b40efcb1ae
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-06-20 15:21:51 +0200

    Remove name and email from localStorage

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -2,7 +2,6 @@ import $ from "./modules/$.js"
 import onboarding from "./modules/onboarding.js"
 import questionnaire from "./modules/questionnaire.js"
 import goals from "./modules/goals.js"
-import login from "./modules/login.js"
 
 // Check if the onboarding page is currently displayed.
 if ($(".onboarding")) {
@@ -17,9 +16,4 @@ if ($(".questionnaire")) {
 // Check if the goals page is currently displayed.
 if ($(".goals_page")) {
     goals()
-}
-
-// Check if the login page is currently displayed.
-if ($(".login_page")) {
-    login()
 }
