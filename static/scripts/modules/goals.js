88aa415a7cd8ca059127d733ba262955aa5b3fa5
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-06-21 16:45:35 +0200

    Rename new_goals.js to goals.js

--- static/scripts/modules/goals.js
+++ static/scripts/modules/goals.js
@@ -1,11 +1,21 @@
 import $ from "./$.js"
 import $$ from "./$$.js"
 
-export default function goals() {
-    let index = 0
-    let goals = []
-    let new_goals = []
-    const saved_goals = JSON.parse(localStorage.getItem("goals")) || []
+export default function new_goals() {
+    // Get the URL parameters.
+    const parameters = new URLSearchParams(window.location.search)
+
+    // Fill the hidden input fields with the URL parameter values.
+    $$(".name").forEach(element => {
+        element.value = parameters.get("name")
+    })
+
+    $$(".email").forEach(element => {
+        element.value = parameters.get("email")
+    })
+
+    // Clear the localStorage.
+    localStorage.clear()
 
     $("#add").addEventListener("click", () => {
         // Show the pop-up.
@@ -17,164 +27,20 @@ export default function goals() {
         $("form").classList.remove("show_popup")
     })
 
-    $(".add_goal").addEventListener("submit", add_goal)
-    $$(".unordered_goal_list").forEach(element => {
-        element.addEventListener("click", increment_repetition)
+    $$(".checkmark_test").forEach((element, index) => {
+        element.addEventListener("click", () => {
+            // Submit the hidden form.
+            $$(".increase_streak")[index].submit()
+
+            // Show the check animation.
+            $$(".checkmark_test")[index].classList.add("checkmark_animation")
+            $$(".checkmark_check_test")[index].classList.add("checkmark_check_animation")
+
+            // $("li").classList.add("confetti")
+        })
+
+        // if complete {
+        // $("confetti_container").classList.remove("hide_state")
+        // }
     })
-    // $(".unordered_goal_list").addEventListener("click", remove_goal)
-
-    // Retrieve the goals from localStorage and render them in the HTML.
-    render_goals(saved_goals, index)
-
-    // Add the goals from localStorage to the goals array.
-    saved_goals.forEach(element => {
-        goals.push(element)
-    })
-
-    delete_state()
-
-    function add_goal(event) {
-        // Prevent the page from reloading.
-        event.preventDefault()
-
-        // Clear the new goals array.
-        new_goals = []
-
-        // Set the index to the goals array length, so that new goals can be addressed correctly.
-        index = goals.length
-
-        // Loop through all checkboxes.
-        $$("input[name=goal]").forEach(checkbox => {
-            // Check if the checkbox is checked.
-            if (checkbox.checked) {
-                // Create a goal object from the input values.
-                const goal = {
-                    name: checkbox.value,
-                    repetition: 0,
-                    completed: false
-                }
-
-                // Add the goal to the goals and new goals arrays.
-                goals.push(goal)
-                new_goals.push(goal)
-            }
-        })
-
-        // Render the new goals in the HTML.
-        render_goals(new_goals, index)
-
-        // Save the goals in localStorage.
-        localStorage.setItem("goals", JSON.stringify(goals))
-
-        // Reset the form.
-        this.reset()
-
-        // Hide the pop-up.
-        $("form").classList.remove("show_popup")
-
-        $$(".unordered_goal_list").forEach(element => {
-            // Remove all previously added eventListeners, to prevent duplicates.
-            element.removeEventListener("click", increment_repetition)
-            element.addEventListener("click", increment_repetition)
-        })
-
-        delete_state()
-    }
-
-    function render_goals(goals, index) {
-        // Add the goals to the bottom of the HTML list.
-        goals.forEach(goal => {
-            $(".unordered_goal_list").insertAdjacentHTML('beforeend', `
-                <li>
-                    <section>
-                        <article> 
-                            <h2>${goal.name}</h2>
-                        </article>
-                        <label for="goal_${index}">
-                            <svg class="checkmark" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 52 52">
-                            <circle class="checkmark_circle" cx="26" cy="26" r="25" fill="none" />
-                            <path class="checkmark_check" fill="none" d="M14.1 27.2l7.1 7.2 16.7-16.8" />
-                        </svg>
-                        </label>
-                    </section>
-                    <div id="goal_progress">
-                        <div></div>
-                    </div>
-                </li>`)
-
-            // Increment the index.
-            index++
-        })
-    }
-
-    function increment_repetition(event) {
-        // Get the number after "goal_" using a substring.
-        const index = event.target.id.substring(5)
-
-        // Increment the repetition.
-        goals[index].repetition++
-
-        // Check if the current repetition is the same as the total amount of repetitions.
-        if (goals[index].repetition == goals[index].total_repetition) {
-            // Set the completed status to true.
-            goals[index].completed = true
-
-            // Change the symbol to a check.
-            $$(".unordered_goal_list label input[type=checkbox]")[index].classList.add("checkmark")
-            // $$("label i")[index].textContent = "✔︎"
-        }
-        // Check if the current repetition exceeds than the total amount of repetitions. 
-        else if (goals[index].repetition > goals[index].total_repetition) {
-            // Change the current repetition to zero.
-            goals[index].repetition = 0
-
-            // Set the completed status to false.
-            goals[index].completed = false
-
-            // Change the symbol back to a plus.
-            $$(".unordered_goal_list label input[type=checkbox]")[index].classList.remove("checkmark")
-            // $$("label i")[index].textContent = "+"
-        }
-
-        // Update the current repetition in the HTML.
-        $$(".repetition_change")[index].textContent = goals[index].repetition
-
-        // Update the goals in localStorage.
-        localStorage.setItem("goals", JSON.stringify(goals))
-
-        // Add a click animation.
-        // $$("label").classList.add(".pulse")
-    }
-
-    // Delete goal.
-    // function remove_goal(event) {
-    //     // If you click on the remove button,
-    //     if (!event.target.matches("button")) return
-    //     const index = event.target.dataset
-
-    //     goal_array.splice(index, 1)
-
-    //     put_goal_in_list_item(goal_array, unordered_goal_list)
-    //     // localStorage.setItem("JSON_all_goals", JSON.stringify(JSON_all_goals))
-    // }
-
-    function delete_state() {
-        if (goals.length > 0) {
-            $(".empty_state").classList.add("hide_state")
-        }
-    }
-
-    // $$(".checkmark").addEventListener("click", () => {
-    //     // Show the check animation
-    //     $$(".checkmark").classList.add("checkmark_animation")
-    //     $$(".checkmark_check").classList.add("checkmark_check_animation")
-    //     // $("li").classList.add("confetti")
-    // })
-
-
 }
-
-/* <input type="checkbox" data-index=${index} name="goal_${index}" id="goal_${index}" ${goal.completed ? "checked" : ""} /> */
-/* <p>
-<span class="repetition_change">${goal.repetition}</span>/21 dagelijks
-</p>   */
--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,8 +1,7 @@
 import $ from "./modules/$.js"
 import onboarding from "./modules/onboarding.js"
 import questionnaire from "./modules/questionnaire.js"
-// import goals from "./modules/goals.js"
-import new_goals from "./modules/new_goals.js"
+import goals from "./modules/goals.js"
 import profile from "./modules/profile.js"
 
 // Check if the onboarding page is currently displayed.
@@ -17,8 +16,7 @@ if ($(".questionnaire")) {
 
 // Check if the goals page is currently displayed.
 if ($(".goals_page")) {
-    // goals()
-    new_goals()
+    goals()
 }
 
 // Check if the profile page is currently displayed.
