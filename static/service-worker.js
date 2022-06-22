0768fef002a7b6c55db696023e2e9938e2279168
Lisanne van Vliet <lisanne.van.vliet@hva.nl>
2022-06-22 22:50:54 +0200

    Improve service worker

--- static/service-worker.js
+++ static/service-worker.js
@@ -44,48 +44,55 @@ const files = [
 ]
 
 // Install the service worker.
-self.addEventListener("install", function (event) {
+self.addEventListener("install", event => {
     event.waitUntil(
-        caches.open("core-cache").then(function (cache) {
-            // Ensure that any new versions of the service worker will take over the page and become activated immediately.
-            self.skipWaiting()
-            return cache.addAll(files)
-        })
+        caches.open("core-cache")
+            .then(cache => {
+                // Ensure that any new versions of the service worker will take over the page and become activated immediately.
+                self.skipWaiting()
+
+                return cache.addAll(files)
+            })
     )
 })
 
-// Activate the service worker.
-self.addEventListener("activate", function (_event) { })
-
 // Fetch the service worker.
-self.addEventListener("fetch", function (event) {
+self.addEventListener("fetch", event => {
     const url = new URL(event.request.url)
+
     // Check if any of the requested files already exists in the core-cache and serve them.
-    if (event.request.method === "GET" && files.includes(url.pathname)) {
+    if (event.request.method == "GET" && files.includes(url.pathname)) {
         event.respondWith(
             caches.open("core-cache")
-                .then(cache => cache.match(event.request.url))
+                .then(cache =>
+                    cache.match(event.request.url)
+                )
         )
-        // Only request the HTML, all the other files are already in the cache.
-    } else if (event.request.method === "GET" && (event.request.headers.get("accept") !== null && event.request.headers.get("accept").includes("text/html"))) {
+    }
+    // Only request the HTML, all the other files are already in the cache.
+    else if (event.request.method == "GET" && (event.request.headers.get("accept") != null && event.request.headers.get("accept").includes("text/html"))) {
         event.respondWith(
             // Stale-while-revalidate (see https://web.dev/offline-cookbook/#stale-while-revalidate).
-            caches.open("dynamic-cache").then(function (cache) {
-                return cache.match(event.request)
-                    // Retrieve, cache and serve the HTML.
-                    .then(function (response) {
-                        var fetchPromise = fetch(event.request).then(function (networkResponse) {
-                            cache.put(event.request, networkResponse.clone())
-                            return networkResponse
+            caches.open("dynamic-cache")
+                .then(cache => {
+                    return cache.match(event.request)
+                        // Retrieve, cache and serve the HTML.
+                        .then(response => {
+                            var fetchPromise = fetch(event.request)
+                                .then(networkResponse => {
+                                    cache.put(event.request, networkResponse.clone())
+                                    return networkResponse
+                                })
+                            return response || fetchPromise
                         })
-                        return response || fetchPromise
-                    })
                     // Serve the offline page if the HTML is not in the cache and there is no internet.
-                    .catch(_error => {
-                        return caches.open("core-cache")
-                            .then(cache => cache.match("/offline"))
-                    })
-            })
+                    // .catch(() => {
+                    //     return caches.open("core-cache")
+                    //         .then(cache =>
+                    //             cache.match("/offline")
+                    //         )
+                    // })
+                })
         )
     }
 })
